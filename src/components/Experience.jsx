cea5e6806cca177ec198fdd997204414392d2a18
FloodzHubGit <florentin.dekneudt@epsi.fr>
2024-05-03 13:08:48 +0200

    feat: Add interactive functionality to Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,7 @@ import { Center, Environment, OrbitControls } from "@react-three/drei";
 import { Card } from "./Card";
 import { myPlayer } from "playroomkit";
 import { useGameEngine } from "../hooks/useGameEngine";
+import { Interactive } from "@react-three/xr";
 
 export const Experience = () => {
   const me = myPlayer();
@@ -14,33 +15,36 @@ export const Experience = () => {
     <>
       <OrbitControls />
 
-      <mesh
-        onClick={(event) => {
+      <Interactive
+        onSelect={(event) => {
           event.stopPropagation();
           if (playerTurn === myIndex) {
             drawCard(myIndex);
           }
         }}
-        position={[0, -3, 0]}
       >
-        <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
+        <mesh position={[0, -3, 0]}>
+          <boxBufferGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Interactive>
 
-      <group rotation={[0, Math.PI, 0]}>
+      <group rotation={[0, Math.PI, 0]} scale={0.5}>
         <Center>
           {cards.map((card, index) => (
-            <Card
-              key={index}
-              type={`${card.type}_${card.value}`}
-              position={[(index - cards.length / 2) * 2, 0, 0]}
-              onClick={(event) => {
-                event.stopPropagation();
-                if (playerTurn === myIndex) {
-                  playCard(myIndex, index);
-                }
-              }}
-            />
+            <Interactive>
+              <Card
+                key={index}
+                type={`${card.type}_${card.value}`}
+                position={[(index - cards.length / 2) * 2, 0, 0]}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  if (playerTurn === myIndex) {
+                    playCard(myIndex, index);
+                  }
+                }}
+              />
+            </Interactive>
           ))}
         </Center>
       </group>
