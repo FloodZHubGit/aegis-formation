0fac3609844968b21a2ff9462f7320fb4c3fd580
Florentin Dekneudt <florentin.dekneudt@videas.fr>
2024-10-07 12:10:44 +0200

    refactor: Add avatars to insertCoin function
    Add an array of avatar image paths to the insertCoin function in order to provide a selection of avatars for the user.

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,26 @@ import "./index.css";
 import { GameEngineProvider } from "./hooks/useGameEngine";
 import { insertCoin } from "playroomkit";
 
+const avatars = [
+  '/images/avatars/avatar_1.png',
+  '/images/avatars/avatar_2.png',
+  '/images/avatars/avatar_3.png',
+  '/images/avatars/avatar_4.png',
+  '/images/avatars/avatar_5.png',
+  '/images/avatars/avatar_6.png',
+  '/images/avatars/avatar_7.png',
+  '/images/avatars/avatar_8.png',
+  '/images/avatars/avatar_9.png',
+  '/images/avatars/avatar_10.png',
+  '/images/avatars/avatar_11.png',
+  '/images/avatars/avatar_12.png',
+  '/images/avatars/avatar_13.png',
+  '/images/avatars/avatar_14.png',
+];
+
 insertCoin({
   // streamMode: true
+  avatars
 }).then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
