cd931e959b196cd93dac003e6544f57caa6581a6
FloodzHubGit <florentin.dekneudt@epsi.fr>
2024-10-15 09:36:17 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import { Incognito } from "./components/Incognito";
+import {
+  isHost,
+  isStreamScreen,
+  myPlayer,
+  useMultiplayerState,
+} from "playroomkit";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Incognito />
-    </>
+  const [chatMessages, setChatMessages] = useMultiplayerState(
+    "chatMessages",
+    []
   );
+  const [newMessage, setNewMessage] = useState("");
+  const me = myPlayer();
+  console.log(me);
+
+  const handleSendMessage = () => {
+    const messageWithName = `${me.state.profile.name}: ${newMessage}`;
+    setChatMessages([...chatMessages, messageWithName]);
+    setNewMessage("");
+  };
+
+  if (isStreamScreen()) {
+    return (
+      <div>
+        <h1>{isHost() ? "Host Stream Screen" : "Guest Stream Screen"}</h1>
+        <ul>
+          {chatMessages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Game Screen</h1>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    );
+  }
 }
 
 export default App;
